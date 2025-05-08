bd236ccc53f090211b269043398710b1ad50631e
구본엽 <piusku2@gmail.com>
2025-05-08 15:00:08 +0900

    [feat] UI 수정 및 버튼음, 배경음악 선택 기능

--- react-typingtrainer/src/App.jsx
+++ react-typingtrainer/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, useLocation } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
+import Click from './assets/mp3/흘릭.mp3';
+import { useEffect } from 'react';
 
 import Header from './components/Header';
 import AppRoutes from './Router';
@@ -8,6 +10,19 @@ import AppRoutes from './Router';
 function AppContent() {
   const location = useLocation();
   const isSignupPage = location.pathname === '/signup';
+
+  useEffect(() => {
+    const audio = new Audio(Click);
+    audio.volume = 0.2;
+    const handleClick = (e) => {
+      if (e.target.tagName === 'BUTTON') {
+        audio.currentTime = 0;
+        audio.play();
+      }
+    };
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, []);
 
   return (
     <div className="app-container">
