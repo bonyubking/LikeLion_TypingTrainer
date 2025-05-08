171a5d473205925a5b0a85b13521d979439a263b
구본엽 <piusku2@gmail.com>
2025-05-08 18:36:43 +0900

    asdf

---
+++ react-typingtrainer/src/components/Header/index.jsx
@@ -0,0 +1,8 @@
+import styles from './Header.module.css';
+
+const Header = () => (
+    <header className={styles.header}>
+    <h1 >Typing Trainer</h1>
+    </header>
+  );
+  export default Header;
